df727d157b1e7fdce656914d85d58ff22939e0f1
valeriialebedieva <valeriia.lebedieva1@gmail.com>
2025-07-02 09:34:17 +0200

    added w1 task

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -39,7 +39,7 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-    // Step 1: Use modulo to handle cases where 'amount' exceeds the size of the list
+        // Step 1: Use modulo to handle cases where 'amount' exceeds the size of the list
         amount %= data.Count;
 
         // Step 2: Slice the list into two parts
