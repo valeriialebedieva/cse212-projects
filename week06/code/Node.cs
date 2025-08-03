0f96d8643ae213cee7f486c27d5debdb5fdabb07
valeriialebedieva <valeriia.lebedieva1@gmail.com>
2025-08-03 11:59:04 +0200

    added w6 task

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,8 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data)
+            return;
 
         if (value < Data)
         {
@@ -33,13 +35,35 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+            
+        if (value < Data)
+        {
+            if (Left == null)
+                return false;
+            return Left.Contains(value);
+        }
+        else
+        {
+            if (Right == null)
+                return false;
+            return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+        
+        if (Left != null)
+            leftHeight = Left.GetHeight();
+            
+        if (Right != null)
+            rightHeight = Right.GetHeight();
+            
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
